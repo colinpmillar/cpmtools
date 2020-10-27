4fe511c74ae269d1f1317ef5e21b51c5689f6654
Colin Millar <colin.millar@ices.dk>
2020-10-27 20:44:14 +0100

    * add write.taf.json() function

---
+++ R/write.taf.json.R
@@ -0,0 +1,36 @@
+#' write.taf.json
+#'
+#' FUNCTION_DESCRIPTION
+#'
+#' @param x PARAM_DESCRIPTION
+#' @param file PARAM_DESCRIPTION, Default: NULL
+#' @param dir PARAM_DESCRIPTION, Default: NULL
+#' @param ... PARAM_DESCRIPTION
+#'
+#' @return no return value
+#'
+#' @details DETAILS
+#'
+#' @seealso
+#'  \code{\link[jsonlite]{read_json}}
+#'
+#' @rdname write.taf.json
+#'
+#' @export
+#'
+#' @importFrom jsonlite write_json
+write.taf.json <- function(x, file = NULL, dir = NULL, ...) {
+  if (is.null(file)) {
+    file <- deparse(substitute(x))
+    file <- sub(".*[@$]", "", file)
+    file <- paste0(file, ".json")
+  }
+  if (!is.null(dir) && file != "") {
+    file <- file.path(sub("[/\\]+$", "", dir), file)
+  }
+  jsonlite::write_json(
+    x,
+    path = file,
+    pretty = TRUE, auto_unbox = TRUE, ...
+  )
+}
