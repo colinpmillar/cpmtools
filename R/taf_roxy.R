ad2d5df340d12d5a13f0b8eb065675b68541730c
colinpmillar <colin.millar@ices.dk>
2020-11-10 23:31:07 +0100

    * add taf_roxy function

---
+++ R/taf_roxy.R
@@ -0,0 +1,33 @@
+#' create a blank data set file
+#'
+#' description
+#'
+#' @param dataset the name of the dataset that will be created
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if (interactive()) {
+#'   # EXAMPLE1
+#' }
+#' }
+#' @rdname taf_roxy
+#' @export
+
+taf_roxy <- function(dataset) {
+  txt <-
+    c(
+      "#' title",
+      "#'",
+      "#' description",
+      "#'",
+      paste0("#' @name ", dataset),
+      "#' @format csv file",
+      "#' @tafOriginator ICES",
+      "#' @tafYear 2020",
+      "#' @tafAccess Public",
+      "#' @tafSource script"
+    )
+
+  cat(txt, sep = "/n", file = taf.boot.path(paste0(dataset, ".R")))
+}
